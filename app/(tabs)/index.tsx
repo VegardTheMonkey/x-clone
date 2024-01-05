7b1235e79d0f301438d088d7363a2439deef818f
VegardTheMonkey <vegardstamadsen@gmail.com>
2024-01-05 22:30:23 +0100

    Build: Implemented new features

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,49 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../../components/EditScreenInfo';
+import { Platform, StyleSheet, RefreshControl, ScrollView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Lottie from 'lottie-react-native';
 import { Text, View } from '../../components/Themed';
+import EditScreenInfo from '../../components/EditScreenInfo';
+import * as React from 'react';
+import { createRandomUser } from '../../utils/generate-dommy-data';
+import { ThreadsContext } from '../../context/thread-context';
+
+
 
 export default function TabOneScreen() {
+  const animationRef = React.useRef<Lottie>(null);
+  const threads = React.useContext(ThreadsContext);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <SafeAreaView>
+      <ScrollView
+        contentContainerStyle={{
+          backgroundColor: "gray",
+          paddingTop: Platform.select({ android: 30 }),
+        }}
+        refreshControl={
+          <RefreshControl
+            refreshing={false}
+            onRefresh={() => {animationRef.current?.play()}}
+            tintColor={"transparent"}
+          />
+        }
+      >
+        <Lottie 
+        ref={animationRef}
+        source={require('../../lottie-animations/threads.json')}
+        loop={false}
+        autoPlay={true}
+        style={{width: 90, height: 90, alignSelf: 'center'}}
+        // onAnimationFinish={() => {
+        //   alert("Animation Finished")
+        // }}
+        
+        />
+
+      {threads.map((thread) => (<Text key={thread.id}>{thread.author.name}</Text>))}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+
